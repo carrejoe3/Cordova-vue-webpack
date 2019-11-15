4fefa4ca443dd231da72efca02ce3d3f79a2c674
Joe <carre.joe3@gmail.com>
2019-11-15 15:26:14 +0000

    installed vue-cordova

--- ToDo/src/main.js
+++ ToDo/src/main.js
@@ -1,11 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueCordova from 'vue-cordova'
 import App from './App'
 import Datetime from 'vue-datetime'
 import 'vue-datetime/dist/vue-datetime.css'
 
 Vue.use(Datetime)
+Vue.use(VueCordova)
 
 Vue.config.productionTip = false
 
