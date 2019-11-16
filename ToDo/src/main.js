5621f8e93987b41455f0818e419a1e684796a988
Joe <carre.joe3@gmail.com>
2019-11-16 18:12:38 +0000

    added new version of todo with a functioning on device ready

--- ToDo/src/main.js
+++ ToDo/src/main.js
@@ -4,22 +4,35 @@ import Vue from 'vue'
 import App from './App'
 import Datetime from 'vue-datetime'
 import 'vue-datetime/dist/vue-datetime.css'
+import VueCordova from 'vue-cordova'
 
+Vue.use(VueCordova)
 Vue.use(Datetime)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+let vueApp = new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  methods: {
+    sendNotification: function () {
+      Vue.cordova.plugins.notification.local.schedule({
+        title: 'My first notification',
+        text: 'Thats pretty easy...',
+        foreground: true
+      })
+      console.log('this is running')
+    }
+  },
+  mounted () {
+    this.sendNotification()
+  }
 })
 
 document.addEventListener('deviceready', onDeviceReady, false)
 
 function onDeviceReady () {
-  alert('this is running')
-  console.log('this is running')
-  document.getElementsByClassName('container').style.backgroundColor = 'red'
+  vueApp.sendNotification()
 }
