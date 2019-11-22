28bafecb43ab3b4020dcb4ab63fdaeaf5c5cb7fd
Joe <carre.joe3@gmail.com>
2019-11-22 15:44:57 +0000

    imported ionic

--- todov5/src/main.js
+++ todov5/src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Ionic from '@ionic/vue'
+import '@ionic/core/css/core.css'
+import '@ionic/core/css/ionic.bundle.css'
 
+Vue.use(Ionic)
 Vue.config.productionTip = false
 Vue.config.ignoredElements = [/^ion-/]
 
