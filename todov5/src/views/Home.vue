b742843807630a222ba63e4b2a82f1e4667b0e13
Joe <carre.joe3@gmail.com>
2019-11-23 12:22:35 +0000

    added todo components from version 1

--- todov5/src/views/Home.vue
+++ todov5/src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div id="home">
+    <CreateToDo v-on:add-todo="addTodo($event)"/>
+    <ToDoList v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { Plugins } from '@capacitor/core'
+import ToDoList from '../components/ToDoList'
+import CreateToDo from '../components/CreateTodo'
 
 const { LocalNotifications } = Plugins
 
@@ -27,6 +30,85 @@ LocalNotifications.schedule({
 
 export default {
   name: 'home',
-  components: {}
+  components: {
+    ToDoList,
+    CreateToDo
+  },
+  methods: {
+    addTodo (toDo) {
+      this.todos.unshift(toDo)
+    }
+  },
+  data () {
+    return {
+      todos: []
+    }
+  },
+  mounted () {
+    if (localStorage.todos) {
+      let savedToDos = JSON.parse(localStorage.todos)
+      this.todos = savedToDos
+    }
+  },
+  watch: {
+    todos (updatedTodos) {
+      localStorage.todos = JSON.stringify(updatedTodos)
+    }
+  }
 }
 </script>
+
+<style>
+#home {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #272822;
+  margin-top: 60px;
+  font-size: 1em;
+}
+.container {
+  margin-bottom: 2%;
+}
+.ui.segment, .ui.button.icon {
+  font-size: 2em;
+}
+.ui.button {
+  font-size: 1em;
+}
+.ui.form .field>label, .ui.form .field>input, .ui.button.createBtn, .ui.button.cancelBtn, .ui.button.closeBtn {
+  font-size: 2em;
+}
+.ui.form .field>input, .ui.form input[type=text].vdatetime-input {
+  font-size: 1.5em;
+}
+.header {
+  font-weight: 700;
+  overflow-wrap: break-word;
+  line-height: initial;
+}
+.header, .meta, .ui.form .field>label, .ui.form .field>input, .ui.button.createBtn, .icons, .ui.form input[type=text].vdatetime-input {
+  margin-bottom: 1em;
+}
+.icons {
+  display: flex;
+  justify-content: space-between;
+}
+.ui.button {
+  background: none;
+  margin-right: 0px;
+  margin-left: 0px;
+  width: 100%;
+}
+.date {
+  font-size: 0.65em;
+}
+.ui.form .field>input.errorField {
+  border: 1px solid #DB2828;
+}
+input[disabled] {
+  border: none;
+  text-align: center;
+}
+</style>
