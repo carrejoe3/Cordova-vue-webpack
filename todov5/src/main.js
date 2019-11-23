357d87b067fdd2fe77f4286d42deb3a67d5f5a89
Joe <carre.joe3@gmail.com>
2019-11-23 12:29:57 +0000

    removed ionic icons and ionic vue

--- todov5/src/main.js
+++ todov5/src/main.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Ionic from '@ionic/vue'
-import '@ionic/core/css/core.css'
-import '@ionic/core/css/ionic.bundle.css'
 
-Vue.use(Ionic)
 Vue.config.productionTip = false
 Vue.config.ignoredElements = [/^ion-/]
 
