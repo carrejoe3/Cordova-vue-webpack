dd5c2f1a64e3f31753ecb671e2b5e5d49c78f08a
Joe <carre.joe3@gmail.com>
2019-11-23 13:51:18 +0000

    installed vue-datetime

--- todov5/src/main.js
+++ todov5/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Datetime from 'vue-datetime'
+import 'vue-datetime/dist/vue-datetime.css'
 import 'semantic-ui-css/semantic.min.css'
 
+Vue.use(Datetime)
 Vue.config.productionTip = false
 Vue.config.ignoredElements = [/^ion-/]
 
