fb2b996fe5fe646dd41355b15057403877aabd4b
Joe Carre <joseph.carre@accessionhealth.com>
2019-12-05 15:14:38 +0000

    added vue-jest

--- App/tests/todo.test.js
+++ App/tests/todo.test.js
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import Todo from '../src/components/Todo'
+import Todo from '../src/components/Todo.vue'
 
 describe('Todo', () => {
     test('is a Todo', () => {
