cd956537064ad096331324a9b82ee2574957849c
Joe Carre <joseph.carre@accessionhealth.com>
2019-12-05 15:20:41 +0000

    installed babel core and jest-serializer-vue

--- App/tests/todo.test.js
+++ App/tests/todo.test.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import Todo from '../src/components/Todo.vue'
 
 describe('Todo', () => {
-    test('is a Todo', () => {
+    test('Is Todo a vue instance?', () => {
         const wrapper = mount(Todo)
         expect(wrapper.isVueInstance()).toBeTruthy()
     })
